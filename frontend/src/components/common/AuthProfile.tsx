adb80d1add0a799f6a9752c3e9cc82b81d9e6322
Youna Joo <you.na.joo0@gmail.com>
2025-11-21 09:47:24 +0900

    feat: 로그인 확인시 email, profileimage, nickname 가져와서 보여주기

--- frontend/src/components/common/AuthProfile.tsx
+++ frontend/src/components/common/AuthProfile.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { LogOut } from "lucide-react";
+import { useAuthStore } from "../../store/authStore";
 
 export default function AuthProfile() {
+  const user = useAuthStore((state) => state.user);
   const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
   const logout = () => {
     console.log("logout");
@@ -14,9 +16,7 @@ export default function AuthProfile() {
           className="flex items-center space-x-2"
         >
           <img
-            src={`https://gravatar.com/avatar/${Math.random()
-              .toString(36)
-              .substring(2)}?d=identicon`}
+            src={user?.profileImage}
             className="w-8 h-8 rounded-full border-2 border-blue-500"
           />
         </button>
@@ -24,8 +24,10 @@ export default function AuthProfile() {
         {isUserMenuOpen && (
           <div className="absolute right-0 mt-2 w-48 bg-white dark:bg-slate-800 rounded-lg shadow-lg py-1 z-50">
             <div className="px-4 py-2 border-b border-gray-100 dark:border-slate-700">
-              <p className="text-sm font-medium">testname</p>
-              <p className="text-xs text-gray-500 dark:text-gray-400">test</p>
+              <p className="text-sm font-medium">{user?.nickname}</p>
+              <p className="text-xs text-gray-500 dark:text-gray-400">
+                {user?.email}
+              </p>
             </div>
             <button
               onClick={() => {
