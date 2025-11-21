5cdcf0252446cf96ce47e9de5718b0e5eb121c77
Youna Joo <you.na.joo0@gmail.com>
2025-11-21 10:40:50 +0900

    feat: 로그인인 유저와 비로그인 유저의 가능한 범위 로직

--- frontend/src/routes/loader/auth.loader.ts
+++ frontend/src/routes/loader/auth.loader.ts
@@ -1,3 +1,4 @@
+import { redirect } from "react-router";
 import { axiosInstance } from "../../api/axios";
 import { useAuthStore } from "../../store/authStore";
 
@@ -12,5 +13,19 @@ export const fetchUserData = async () => {
     }
   } catch (e) {
     console.error(e);
+  }
+};
+
+export const requireAuth = () => {
+  const token = sessionStorage.getItem("access_token");
+  if (!token) {
+    return redirect("/auth/login");
+  }
+};
+
+export const redirectIfAuth = () => {
+  const token = sessionStorage.getItem("access_token");
+  if (token) {
+    return redirect("/");
   }
 };
