984bc8dfd5063436ddec7ebee47694be0af5e4b9
Youna Joo <you.na.joo0@gmail.com>
2025-12-10 21:13:39 +0900

    feat: 내가 작성한 글만 edit 버튼 보이게 로직

--- frontend/src/routes/pages/posts/PostRead.tsx
+++ frontend/src/routes/pages/posts/PostRead.tsx
@@ -6,10 +6,12 @@ import CommentForm from "../../../components/comment/CommentForm";
 import CommentComponent from "../../../components/comment/CommentComponent";
 import { useLoaderData } from "react-router";
 import { format } from "date-fns";
+import { useAuthStore } from "../../../store/authStore";
 
 export default function PostRead() {
   const { post, relatedPosts }: { post: Post; relatedPosts: Post[] } =
     useLoaderData();
+  const user = useAuthStore((state) => state.user);
   const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
 
   // 게시글이 없으면
@@ -31,16 +33,18 @@ export default function PostRead() {
           <span className="inline-block bg-blue-500 text-white text-sm font-semibold px-3 py-1 rounded-md">
             {post.category}
           </span>
-          <div className="flex gap-2">
-            <button className="flex items-center gap-2 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors">
-              <Pencil className="w-4 h-4" />
-              Edit
-            </button>
-            <button className="flex items-center gap-2 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition-colors">
-              <Trash2 className="w-4 h-4" />
-              Delete
-            </button>
-          </div>
+          {post.author._id === user?.id && (
+            <div className="flex gap-2">
+              <button className="flex items-center gap-2 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors cursor-pointer">
+                <Pencil className="w-4 h-4" />
+                Edit
+              </button>
+              <button className="flex items-center gap-2 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition-colors cursor-pointer">
+                <Trash2 className="w-4 h-4" />
+                Delete
+              </button>
+            </div>
+          )}
         </div>
 
         <h1 className="text-2xl lg:text-3xl font-bold text-white mb-6">
