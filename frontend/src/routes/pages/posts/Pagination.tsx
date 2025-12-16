261324d98b176b5c7a0d8642563f52663a2bdc05
Youna Joo <you.na.joo0@gmail.com>
2025-12-16 23:31:35 +0900

    feat: 범용적 페이지네이션 컴포넌트 구현

--- frontend/src/routes/pages/posts/Pagination.tsx
+++ frontend/src/routes/pages/posts/Pagination.tsx
@@ -6,7 +6,7 @@ export default function Pagination({
   maxPage,
   onPageChange,
 }: {
-  papageRange: number;
+  pageRange: number;
   currentPage: number;
   maxPage: number;
   onPageChange: (page: number) => void;
@@ -32,9 +32,41 @@ export default function Pagination({
     }
     return arr;
   }, [currentPage, half, maxPage, pageRange]);
+
   return (
     <>
-      
+      <div className="flex justify-center mt-12 gap-2">
+        <button
+          className="px-4 py-2 rounded-lg bg-gray-100 dark:bg-slate-800 text-gray-700 dark:text-gray-300 disabled:opacity-50 cursor-pointer"
+          onClick={() => onPageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          &lt;
+        </button>
+
+        {pages.map((page) => (
+          <button
+            key={page}
+            className={
+              page === currentPage
+                ? "w-10 h-10 rounded-lg cursor-pointer bg-blue-500 text-white"
+                : "w-10 h-10 rounded-lg cursor-pointer bg-gray-100 dark:bg-slate-800 text-gray-700 dark:text-gray-300"
+            }
+            onClick={() => onPageChange(page)}
+          >
+            {page}
+          </button>
+        ))}
+
+        {/* 다음 버튼 */}
+        <button
+          className="px-4 py-2 rounded-lg bg-gray-100 dark:bg-slate-800 text-gray-700 dark:text-gray-300 disabled:opacity-50 cursor-pointer"
+          onClick={() => onPageChange(currentPage + 1)}
+          disabled={currentPage === maxPage}
+        >
+          &gt;
+        </button>
+      </div>
     </>
   );
 }
