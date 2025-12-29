278c5b823e4854b82aed13744f43ca283101dac9
Youna Joo <you.na.joo0@gmail.com>
2025-12-29 23:32:17 +0900

    feat: refreshToken 재발급 로직 구현

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -16,3 +16,25 @@ axiosInstance.interceptors.request.use(
   },
   (error) => Promise.reject(error)
 );
+
+let retry = false;
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response && error.response.status === 401 && !retry) {
+      retry = true;
+      try {
+        const res = await axiosInstance.post("/auth/refresh");
+        if (!res.data.accessToken) throw new Error("Access token is missing");
+        retry = false;
+        sessionStorage.setItem("access_token", res.data.accessToken);
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        sessionStorage.removeItem("access_token");
+        await axiosInstance.post("/auth/logout");
+        return Promise.reject(refreshError);
+      }
+    }
+  }
+);
